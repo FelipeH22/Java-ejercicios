86cf3059cb474e57f3d97444d6f5fc3c5c5e2ec4
Juan Felipe Herrera Poveda <jfherrerap@correo,udistrital.edu.co>
2018-08-16 19:09:35 -0500

    Ejercicios Básicos

--- Dos/src/ejercicios_basicos/Ciento_cuatro.java
+++ Dos/src/ejercicios_basicos/Ciento_cuatro.java
@@ -1,14 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicios_basicos;
+import java.util.Scanner;
 
-/**
- *
- * @author Felipe
- */
-public class Ciento_cuatro {
-    
+public class Ciento_cuatro 
+{
+    public int cantidad;
+    class persona
+    {
+        String nombre;
+        String sexo;
+        int edad;
+        float peso;
+        String color_de_pelo; 
+        String color_de_piel;  
+        String color_de_ojos;
+        String nacionalidad;
+        double telefono;
+        public void cantidad_datos()
+        {
+            Scanner sc=new Scanner (System.in);
+            System.out.println("Digite la cantidad de personas a registrar");
+            cantidad=sc.nextInt();        
+        }
+        public void nombref(String name)
+        {
+            this.nombre=name;
+        }
+        public void sexof(String sex)
+        {
+            this.sexo=sex;
+        }
+        public void edadf(int age)
+        {
+            this.edad=age;
+        }
+        public void pesof(float w)
+        {
+            this.peso=w;
+        }
+        public void color_de_pelof(String hcolor)
+        {
+            this.color_de_pelo=hcolor;
+        }
+        public void color_de_pielf(String scolor)
+        {
+            this.color_de_piel=scolor;
+        }
+        public void color_de_ojosf(String ecolor)
+        {
+            this.color_de_ojos=ecolor;
+        }
+        public void nacionalidadf(String nationality)
+        {
+            this.nacionalidad=nationality;
+        }
+         public void telefonof(double phone)
+        {
+            this.telefono=phone;
+        }
+         
+    }
 }
+
+   
--- Dos/src/ejercicios_basicos/Ciento_cuatro_main.java
+++ Dos/src/ejercicios_basicos/Ciento_cuatro_main.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicios_basicos;
+import ejercicios_basicos.Ciento_cuatro;
+import java.util.Scanner;
 
-/**
- *
- * @author Felipe
- */
-public class Ciento_cuatro_main {
-    
+public class Ciento_cuatro_main 
+{
+    public static void main(String[] args)
+    {
+        Ciento_cuatro b = new Ciento_cuatro();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Ingrese el nombre");
+        b.setnombref(sc.nextLine());
+        System.out.println("Ingrese el sexo");
+        b.setsexof(sc.nextLine());
+        System.out.println("Ingrese la edad");
+        b.setedadf(sc.nextInt());
+        System.out.println("Ingrese el peso en kg");
+        b.setpesof(sc.nextFloat());
+        System.out.println("Ingrese el color de pelo");
+        b.setcolor_de_pelof(sc.nextLine());
+        System.out.println("Ingrese el color de piel");
+        b.setcolor_de_pielf(sc.nextLine());
+        System.out.println("Ingrese el color de ojos");
+        b.setcolor_de_ojosf(sc.nextLine());
+        
+        
+        
+    }        
 }
